9c518644335be347f4249978f768393a8457675a
Zac Turner <zac@oneblink.io>
2020-07-30 16:54:15 +1000

    ON-7247 # Fixed types not bundling with publish

--- typescript/misc.d.ts
+++ typescript/misc.d.ts
@@ -1,1 +1,30 @@
 export type NoU = null | undefined
+
+export interface UserProfile {
+  isSAMLUser: boolean
+  providerType: string
+  providerUserId: string
+  userId: string
+  username: string
+  email: string | NoU
+  firstName: string | NoU
+  lastName: string | NoU
+  fullName: string | NoU
+  picture: string | NoU
+  role: string | NoU
+  supervisor:
+    | {
+        fullName: string | NoU
+        email: string | NoU
+        providerUserId: string | NoU
+      }
+    | NoU
+}
+
+export interface QueryParameters {
+  [property: string]: string | Array<string | number> | null
+}
+
+export interface GenericObject {
+  [property: string]: unknown
+}

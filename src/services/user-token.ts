8443caf4c3a2e13e9313a2ffd688cf0befc3f5ab
kizaonline <kizaonline@gmail.com>
2020-11-23 10:17:14 +1100

    ON-9173 # Remove default

--- src/services/user-token.ts
+++ src/services/user-token.ts
@@ -1,5 +1,3 @@
-import { parseQueryString } from './query-string'
-
 import { MiscTypes } from '@oneblink/types'
 let userToken: string | MiscTypes.NoU = null
 
@@ -8,14 +6,5 @@ export function setUserToken(token: string | MiscTypes.NoU): void {
 }
 
 export function getUserToken() {
-  if (userToken) {
-    return userToken
-  }
-  return getQueryStringToken()
-}
-
-function getQueryStringToken() {
-  const query = parseQueryString(window.location.search.substring(1))
-
-  return query.userToken
+  return userToken
 }

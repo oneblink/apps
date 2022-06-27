21fac8d18683032521b67f62baa41b2d6da547ac
Matt Carroll <matt.carroll139@gmail.com>
2022-06-27 12:18:01 +1000

    ON-22712 # Added functions to manage approval additional notes

--- src/approvals-service.ts
+++ src/approvals-service.ts
@@ -1,6 +1,7 @@
 import OneBlinkAppsError from './services/errors/oneBlinkAppsError'
 import { isOffline } from './offline-service'
 import {
+  deleteRequest,
   getRequest,
   putRequest,
   searchRequest,
@@ -42,16 +43,11 @@ export type FormSubmissionApprovalResponse = {
 }
 
 export type FormSubmissionsAdministrationApprovalsResponse = {
-  approvals: Array<{
-    formSubmissionMeta: SubmissionTypes.FormSubmissionMeta
-    formApprovalFlowInstance: ApprovalTypes.FormApprovalFlowInstance
-    formSubmissionApprovals: ApprovalTypes.FormSubmissionApproval[]
-    history: Array<{
-      formSubmissionMeta: SubmissionTypes.FormSubmissionMeta
-      formApprovalFlowInstance: ApprovalTypes.FormApprovalFlowInstance
-      formSubmissionApprovals: ApprovalTypes.FormSubmissionApproval[]
-    }>
-  }>
+  approvals: Array<
+    FormApprovalFlowInstanceHistory & {
+      history: Array<FormApprovalFlowInstanceHistory>
+    }
+  >
   meta: {
     limit: number
     offset: number
@@ -816,4 +812,292 @@ export async function submitApprovalForm({
     }
   }
   return formSubmissionResult
+}
+
+/**
+ * Create an additional note for a form submission approval.
+ *
+ * #### Example
+ *
+ * ```js
+ * const formSubmissionApprovalId = 'd96ff812-f263-4a66-868d-e259a1fe4157'
+ * const newFormSubmissionApprovalNote = {
+ *   note: 'this is my note',
+ * }
+ * const createdFormSubmissionApprovalNote =
+ *   await approvalsService.createApprovalAdditionalNote({
+ *     formSubmissionApprovalId,
+ *     newFormSubmissionApprovalNote,
+ *   })
+ * ```
+ *
+ * @param options
+ * @returns
+ */
+export async function createApprovalAdditionalNote({
+  formSubmissionApprovalId,
+  newFormSubmissionApprovalNote,
+  abortSignal,
+}: {
+  formSubmissionApprovalId: string
+  newFormSubmissionApprovalNote: ApprovalTypes.NewFormSubmissionApprovalNote
+  abortSignal?: AbortSignal
+}): Promise<ApprovalTypes.FormSubmissionApprovalNote> {
+  try {
+    return await postRequest<ApprovalTypes.FormSubmissionApprovalNote>(
+      `${tenants.current.apiOrigin}/form-submission-approvals/${formSubmissionApprovalId}/additional-notes`,
+      newFormSubmissionApprovalNote,
+      abortSignal,
+    )
+  } catch (err) {
+    console.error('Error creating form submission approval note', err)
+    if (abortSignal?.aborted) {
+      throw err
+    }
+
+    Sentry.captureException(err)
+
+    const error = err as HTTPError
+    if (isOffline()) {
+      throw new OneBlinkAppsError(
+        'You are currently offline, please connect to the internet and try again',
+        {
+          originalError: error,
+          isOffline: true,
+        },
+      )
+    }
+    switch (error.status) {
+      case 401: {
+        throw new OneBlinkAppsError(
+          'You cannot create additional notes without first logging in. Please login and try again.',
+          {
+            originalError: error,
+            requiresLogin: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 403: {
+        throw new OneBlinkAppsError(
+          'You do not have access to this application. Please contact your administrator to gain the correct level of access.',
+          {
+            originalError: error,
+            requiresAccessRequest: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 400:
+      case 404: {
+        throw new OneBlinkAppsError(error.message, {
+          title: 'Invalid Request',
+          httpStatusCode: error.status,
+        })
+      }
+      default: {
+        throw new OneBlinkAppsError(
+          'An unknown error has occurred. Please contact support if the problem persists.',
+          {
+            originalError: error,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+    }
+  }
+}
+
+/**
+ * Update an additional note for a form submission approval.
+ *
+ * #### Example
+ *
+ * ```js
+ * const formSubmissionApprovalId = 'd96ff812-f263-4a66-868d-e259a1fe4157'
+ * const formSubmissionApprovalNote = {
+ *   id: '1e84dee3-0dc0-4abe-b2cf-d15d62fd9d0b',
+ *   note: 'this is my note',
+ *   createdAt: '2022-06-24T03:16:34.696Z',
+ *   createdBy: {
+ *     username: 'user@domain.com',
+ *   },
+ *   updatedAt: '2022-06-24T03:16:34.696Z',
+ *   lastUpdatedBy: {
+ *     username: 'user@domain.com',
+ *   },
+ * }
+ * const updatedFormSubmissionApprovalNote =
+ *   await approvalsService.updateApprovalAdditionalNote({
+ *     formSubmissionApprovalId,
+ *     formSubmissionApprovalNote,
+ *   })
+ * ```
+ *
+ * @param options
+ * @returns
+ */
+export async function updateApprovalAdditionalNote({
+  formSubmissionApprovalId,
+  formSubmissionApprovalNote,
+  abortSignal,
+}: {
+  formSubmissionApprovalId: string
+  formSubmissionApprovalNote: ApprovalTypes.FormSubmissionApprovalNote
+  abortSignal?: AbortSignal
+}): Promise<ApprovalTypes.FormSubmissionApprovalNote> {
+  try {
+    return await putRequest<ApprovalTypes.FormSubmissionApprovalNote>(
+      `${tenants.current.apiOrigin}/form-submission-approvals/${formSubmissionApprovalId}/additional-notes/${formSubmissionApprovalNote.id}`,
+      formSubmissionApprovalNote,
+      abortSignal,
+    )
+  } catch (err) {
+    console.error('Error updating form submission approval note', err)
+    if (abortSignal?.aborted) {
+      throw err
+    }
+
+    Sentry.captureException(err)
+
+    const error = err as HTTPError
+    if (isOffline()) {
+      throw new OneBlinkAppsError(
+        'You are currently offline, please connect to the internet and try again',
+        {
+          originalError: error,
+          isOffline: true,
+        },
+      )
+    }
+    switch (error.status) {
+      case 401: {
+        throw new OneBlinkAppsError(
+          'You cannot update additional notes without first logging in. Please login and try again.',
+          {
+            originalError: error,
+            requiresLogin: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 403: {
+        throw new OneBlinkAppsError(
+          'You do not have access to this application. Please contact your administrator to gain the correct level of access.',
+          {
+            originalError: error,
+            requiresAccessRequest: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 400:
+      case 404: {
+        throw new OneBlinkAppsError(error.message, {
+          title: 'Invalid Request',
+          httpStatusCode: error.status,
+        })
+      }
+      default: {
+        throw new OneBlinkAppsError(
+          'An unknown error has occurred. Please contact support if the problem persists.',
+          {
+            originalError: error,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+    }
+  }
+}
+
+/**
+ * Delete an additional note for a form submission approval.
+ *
+ * #### Example
+ *
+ * ```js
+ * const formSubmissionApprovalId = 'd96ff812-f263-4a66-868d-e259a1fe4157'
+ * const formSubmissionApprovalNoteId = '1e84dee3-0dc0-4abe-b2cf-d15d62fd9d0b',
+ * const updatedFormSubmissionApprovalNote =
+ *   await approvalsService.deleteApprovalAdditionalNote({
+ *     formSubmissionApprovalId,
+ *     formSubmissionApprovalNoteId,
+ *   })
+ * ```
+ *
+ * @param options
+ * @returns
+ */
+export async function deleteApprovalAdditionalNote({
+  formSubmissionApprovalId,
+  formSubmissionApprovalNoteId,
+  abortSignal,
+}: {
+  formSubmissionApprovalId: string
+  formSubmissionApprovalNoteId: string
+  abortSignal?: AbortSignal
+}): Promise<void> {
+  try {
+    return await deleteRequest(
+      `${tenants.current.apiOrigin}/form-submission-approvals/${formSubmissionApprovalId}/additional-notes/${formSubmissionApprovalNoteId}`,
+      abortSignal,
+    )
+  } catch (err) {
+    console.error('Error updating form submission approval note', err)
+    if (abortSignal?.aborted) {
+      throw err
+    }
+
+    Sentry.captureException(err)
+
+    const error = err as HTTPError
+    if (isOffline()) {
+      throw new OneBlinkAppsError(
+        'You are currently offline, please connect to the internet and try again',
+        {
+          originalError: error,
+          isOffline: true,
+        },
+      )
+    }
+    switch (error.status) {
+      case 401: {
+        throw new OneBlinkAppsError(
+          'You cannot delete additional notes without first logging in. Please login and try again.',
+          {
+            originalError: error,
+            requiresLogin: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 403: {
+        throw new OneBlinkAppsError(
+          'You do not have access to this application. Please contact your administrator to gain the correct level of access.',
+          {
+            originalError: error,
+            requiresAccessRequest: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 400:
+      case 404: {
+        throw new OneBlinkAppsError(error.message, {
+          title: 'Invalid Request',
+          httpStatusCode: error.status,
+        })
+      }
+      default: {
+        throw new OneBlinkAppsError(
+          'An unknown error has occurred. Please contact support if the problem persists.',
+          {
+            originalError: error,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+    }
+  }
 }

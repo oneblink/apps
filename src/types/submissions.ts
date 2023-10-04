199905082b23e38bf4bb8a78cb623d9f303e4c58
David Porter <david@oneblink.io>
2023-10-04 12:28:56 +1100

    ON-35105 # add doc strings

--- src/types/submissions.ts
+++ src/types/submissions.ts
@@ -57,7 +57,9 @@ export type FormSubmission = DraftSubmission &
     previousFormSubmissionApprovalId?: string
     /** The id of the scheduled task being completed */
     taskId?: number
+    /** The id of the scheduled task group the task is associated with */
     taskGroupId?: number
+    /** The id of the scheduled task group instance the task is associated with */
     taskGroupInstanceId?: number
   }
 

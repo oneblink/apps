86b9fe5ca9405d7a2f33dfed5c9651bf5e428bc7
kizaonline <kizaonline@gmail.com>
2025-01-22 11:52:16 +1100

    ON-46332 # Add `approvalsService.getFormSubmissionApprovalStatus()`

--- src/approvals-service.ts
+++ src/approvals-service.ts
@@ -1067,3 +1067,85 @@ export async function deleteApprovalAdditionalNote({
     }
   }
 }
+
+/**
+ * Get the status of a single FormSubmissionApproval for the given id.
+ *
+ * #### Example
+ *
+ * ```js
+ * const formSubmissionApprovalId = 'd27966cc-128d-48a2-b681-6ad52012e113'
+ * const { status } =
+ *   await approvalsService.getFormSubmissionApprovalStatus(
+ *     formSubmissionApprovalId,
+ *   )
+ * ```
+ *
+ * @param formSubmissionApprovalId
+ * @param abortSignal
+ * @returns
+ */
+export async function getFormSubmissionApprovalStatus(
+  formSubmissionApprovalId: string,
+  abortSignal?: AbortSignal,
+): Promise<FormSubmissionApprovalResponse> {
+  try {
+    const result = await getRequest<FormSubmissionApprovalResponse>(
+      `${tenants.current.apiOrigin}/form-submission-approvals/${formSubmissionApprovalId}/status`,
+      abortSignal,
+    )
+    return result
+  } catch (err) {
+    Sentry.captureException(err)
+    console.error('Error retrieving form submission approval status', err)
+
+    const error = err as HTTPError
+    if (isOffline()) {
+      throw new OneBlinkAppsError(
+        'You are currently offline and do not have a local copy of this app available, please connect to the internet and try again',
+        {
+          originalError: error,
+          isOffline: true,
+        },
+      )
+    }
+    switch (error.status) {
+      case 401: {
+        throw new OneBlinkAppsError(
+          'You cannot action this pending submission without first logging in. Please login and try again.',
+          {
+            originalError: error,
+            requiresLogin: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 403: {
+        throw new OneBlinkAppsError(
+          'You do not have access to this application. Please contact your administrator to gain the correct level of access.',
+          {
+            originalError: error,
+            requiresAccessRequest: true,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+      case 400:
+      case 404: {
+        throw new OneBlinkAppsError(error.message, {
+          title: 'Invalid Request',
+          httpStatusCode: error.status,
+        })
+      }
+      default: {
+        throw new OneBlinkAppsError(
+          'An unknown error has occurred. Please contact support if the problem persists.',
+          {
+            originalError: error,
+            httpStatusCode: error.status,
+          },
+        )
+      }
+    }
+  }
+}

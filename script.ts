99b8ea6ff887ee00725a4d87be1ad0f529ab66ba
Zac Turner <zac@oneblink.io>
2025-03-28 10:55:59 +1100

    [RELEASE] 22.0.0-beta.4

--- script.ts
+++
@@ -1,8 +0,0 @@
-import { downloadPreFillFormData } from './src/services/api/prefill'
-
-const fn = async () => {
-  const result = await downloadPreFillFormData(1, '')
-  console.log(result)
-}
-
-fn()
